68643e5add55716a6f6b232b4b6f24683f0a889b
Ayush Tyagi <54800927+ayushtyagi01@users.noreply.github.com>
2023-09-26 16:05:53 +0530

    Update Navbar.js

--- Frontend/plant-nursery/src/Navbar.js
+++ Frontend/plant-nursery/src/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
                 aria-current="page"
                 href="/"
               >
-                Home
+                Home1
               </a>
               <a
                 className={`nav-link ${
